174477896f2fcb8966d9d461063de411e5130f59
Arthur Lage <arthurlage2006@gmail.com>
2023-01-01 23:22:07 -0300

    create modal, pledges

--- main.js
+++ main.js
@@ -1,9 +1,50 @@
-const hamburgerButton = document.querySelector(".hamburger")
-const menuHeader = document.querySelector("header")
-const overlay = document.querySelector(".overlay")
+const hamburgerButton = document.querySelector(".hamburger");
+const menuHeader = document.querySelector("header");
+const overlay = document.querySelector(".overlay");
+const pledges = document.querySelectorAll(".pledges");
+const modalClasses = ["modal__choice", "modal__success"];
+
+let currentModalStep = "modal__choice";
+
+function updateModal() {
+  document.body.classList.remove("menu-active");
+  clearModal();
+  document.body.classList.add("menu-active");
+  showModal(currentModalStep);
+}
+
+function closeModal() {
+  overlay.classList.remove("active");
+  document.body.classList.remove("menu-active");
+  clearModal();
+}
+
+function clearModal() {
+  modalClasses.forEach((className) => {
+    document.querySelector(`.${className}`).classList.remove("active");
+  });
+}
+
+function showModal(modalClassName) {
+  document.body.classList.add("menu-active");
+  overlay.classList.add("active");
+  document.querySelector(`.${modalClassName}`).classList.add("active");
+}
+
+function clearPledges() {
+  pledges.forEach((currentPledge) => {
+    currentPledge.classList.remove("selected");
+  });
+}
 
 hamburgerButton.addEventListener("click", () => {
-    document.body.classList.toggle("menu-active")
-    menuHeader.classList.toggle("menu-active")
-    overlay.classList.toggle("active")
-})
+  document.body.classList.toggle("menu-active");
+  menuHeader.classList.toggle("menu-active");
+  overlay.classList.toggle("active");
+});
+
+pledges.forEach((currentPledge) => {
+  currentPledge.addEventListener("click", (e) => {
+    e.target.classList.add("selected");
+  });
+});
